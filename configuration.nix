cb2a78219280720143a3a0ba8fa080c039ee7b01
Renan <7enanmail@proton.me>
2025-12-23 15:27:47 -0300

    Localization settings

--- configuration.nix
+++ configuration.nix
@@ -10,26 +10,10 @@
       ./hardware-configuration.nix
       ./boot/default.nix
       ./networking/default.nix
+      ./localization/default.nix
     ];
 
   services.udisks2.enable = true;
-  # Set your time zone.
-  time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "pt_BR.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
@@ -57,15 +41,6 @@
   ];
 
   services.blueman.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
---
+++ localization/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ...}:
+{
+  
+  # Timezone
+  time.timeZone = "America/Sao_Paulo";
+
+  #locale
+  i18n = {
+    defaultLocale = "pt_BR.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+  };
+  
+  # Console keymap
+  console.keyMap = "br-abnt2";
+
+  # X11 keyboard
+  services.xserver = {
+    xkb = {
+      layout = "br";
+      variant = "abnt2";
+    };
+    excludePackages = with pkgs; [
+      xterm
+    ];
+  };
+}
